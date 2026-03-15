ca6a209c29651e27e2bda665a274cbff71e821c8
AymenFennani <fennani.aymen@gmail.com>
2026-03-15 06:58:32 -0400

    feat: acrylic UI with glass cards, Mica/Acrylic backdrop, and layered transparency

--- TicTacToe/Platforms/Windows/App.xaml.cs
+++ TicTacToe/Platforms/Windows/App.xaml.cs
@@ -1,24 +1,35 @@
-﻿using Microsoft.UI.Xaml;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+﻿using Microsoft.UI.Composition.SystemBackdrops;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Media;
 
 namespace TicTacToe.WinUI;
 
-/// <summary>
-/// Provides application-specific behavior to supplement the default Application class.
-/// </summary>
 public partial class App : MauiWinUIApplication
 {
-	/// <summary>
-	/// Initializes the singleton application object.  This is the first line of authored code
-	/// executed, and as such is the logical equivalent of main() or WinMain().
-	/// </summary>
 	public App()
 	{
 		this.InitializeComponent();
 	}
 
 	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
+	protected override void OnLaunched(LaunchActivatedEventArgs args)
+	{
+		base.OnLaunched(args);
+
+		if (MauiWinUIApplication.Current.Application.Windows.Count > 0 &&
+		    MauiWinUIApplication.Current.Application.Windows[0].Handler?.PlatformView is MauiWinUIWindow winUIWindow)
+		{
+			try
+			{
+				winUIWindow.SystemBackdrop = new MicaBackdrop { Kind = MicaKind.BaseAlt };
+			}
+			catch
+			{
+				try { winUIWindow.SystemBackdrop = new DesktopAcrylicBackdrop(); }
+				catch { /* backdrop not supported on this system */ }
+			}
+		}
+	}
 }
 
