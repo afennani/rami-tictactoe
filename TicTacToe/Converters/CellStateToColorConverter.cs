b55c1b60c63804bf882ae9203f456fa677113ead
AymenFennani <fennani.aymen@gmail.com>
2026-03-15 07:19:22 -0400

    feat: neumorphism UI - soft extruded cards, light gray palette, accent buttons

--- TicTacToe/Converters/CellStateToColorConverter.cs
+++ TicTacToe/Converters/CellStateToColorConverter.cs
@@ -8,10 +8,10 @@ public class CellStateToColorConverter : IValueConverter
     public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture) =>
         value is CellState s ? s switch
         {
-            CellState.X => Color.FromArgb("#5563B3F9"),
-            CellState.O => Color.FromArgb("#55F472B6"),
-            _ => Color.FromArgb("#20FFFFFF")
-        } : Color.FromArgb("#20FFFFFF");
+            CellState.X => Color.FromArgb("#6C63FF"),
+            CellState.O => Color.FromArgb("#F687B3"),
+            _ => Color.FromArgb("#E0E5EC")
+        } : Color.FromArgb("#E0E5EC");
 
     public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) =>
         throw new NotImplementedException();
