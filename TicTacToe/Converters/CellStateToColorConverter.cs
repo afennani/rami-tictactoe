1bda184ac82e4a6f8c36ed3d14ca5b060f914095
AymenFennani <fennani.aymen@gmail.com>
2026-03-15 07:05:28 -0400

    feat: glassmorphism design - gradient bg, frosted glass cards, translucent cells

--- TicTacToe/Converters/CellStateToColorConverter.cs
+++ TicTacToe/Converters/CellStateToColorConverter.cs
@@ -8,10 +8,10 @@ public class CellStateToColorConverter : IValueConverter
     public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture) =>
         value is CellState s ? s switch
         {
-            CellState.X => Color.FromArgb("#2196F3"),
-            CellState.O => Color.FromArgb("#F44336"),
-            _ => Color.FromArgb("#E8E8E8")
-        } : Color.FromArgb("#E8E8E8");
+            CellState.X => Color.FromArgb("#5563B3F9"),
+            CellState.O => Color.FromArgb("#55F472B6"),
+            _ => Color.FromArgb("#20FFFFFF")
+        } : Color.FromArgb("#20FFFFFF");
 
     public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) =>
         throw new NotImplementedException();
